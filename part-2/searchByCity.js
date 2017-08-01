69278f55532b76caa80da1cafa140b8970f10f96
Jose Moreno <morenojr90@yahoo.com>
2017-07-31 21:20:02 -0700

    Finish part 2 functions

--- part-2/searchByCity.js
+++ part-2/searchByCity.js
@@ -3,5 +3,8 @@ const clients = require('./clients.json');
 
 
     for(var key in clients) {
-
+    var toUp = clients[key].city
+    if(toUp.toUpperCase() == name.toUpperCase())
+      console.log("ID:" + clients[key].id + ' - REP NAME:' + clients[key].rep_name + '- COMPANY:' + clients[key].company +
+      " - CITY:" + clients[key].city + ' - STATE:' + clients[key].state);
     };
--- part-2/searchByCompany.js
+++ part-2/searchByCompany.js
@@ -2,5 +2,9 @@ const clients = require('./clients.json');
   let name = process.argv[2];
   console.log("Finding companies with name " + "\"" + nameUp + "\"" + "...")
   for(var key in clients) {
-
+    var valueUp = clients[key].company
+    var nameUp = name.toUpperCase();
+    if(valueUp.toUpperCase().startsWith(nameUp)) {
+      console.log("ID: " + clients[key].id + ' - ' + "COMPANY: " + clients[key].company + ' - ' + "PHONE: " + clients[key].phone);
+    }
   };
