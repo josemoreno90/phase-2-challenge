8dde7acabd284abc6a702794dc02ae2a87e83f9f
Jose Moreno <morenojr90@yahoo.com>
2017-07-31 21:18:21 -0700

    Create searchByCompany & searchByCity empty functions

---
+++ part-2/searchByCity.js
@@ -0,0 +1,7 @@
+const clients = require('./clients.json');
+  let name = process.argv[2];
+
+
+    for(var key in clients) {
+
+    };
---
+++ part-2/searchByCompany.js
@@ -0,0 +1,6 @@
+const clients = require('./clients.json');
+  let name = process.argv[2];
+  console.log("Finding companies with name " + "\"" + nameUp + "\"" + "...")
+  for(var key in clients) {
+
+  };
